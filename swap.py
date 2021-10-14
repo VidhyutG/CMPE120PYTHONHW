3f286e55bab2804e5ad4a529d21770b928a9d9f3
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 15:37:21 -0700

    fixed indentation

--- swap.py
+++ swap.py
@@ -1,7 +1,7 @@
 #program to swap first and last item in a list
 def swap_last_item(newList):
-''' Accepts a list and interchanges the first and last element in the list'''
-#length of list is assigned to size for easy index access
+ ''' Accepts a list and interchanges the first and last element in the list'''
+ #length of list is assigned to size for easy index access
   size = len(newList) 
   temp = newList[0]
   newList[0] = newList[size-1]

763cf985f3e3a1a36d25735ddc58ffd9832e68aa
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 15:48:02 -0700

    fixed indentations and changed code for division

--- calculator.py
+++ calculator.py
@@ -12,8 +12,6 @@ def calculator(number1,number2,operator):
   print(number1*number2)
 # else if operator is "/",then perform division and print result
  elif operator=="/":
-    if(number2==0)
-     print("no division by 0")
   print(number1/number2)
 # else if operator is "//",then perform integer division and print result
  elif operator=="//":

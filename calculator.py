3fa594b54c0b4a9b85fec41b168908c1383e30ea
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 20:28:16 -0700

    updated calculator.py with return

--- calculator.py
+++ calculator.py
@@ -3,22 +3,22 @@ def calculator(number1,number2,operator):
  '''Function accepts 2 numbers and an operator , based on the operator given it will perform the arithmetic operation'''
 # if operator is "+",then perform addition and print result
  if operator=="+":
-  print(number1+number2)
+  return number1+number2
 # else if operator is "-",then perform subtraction and print result
  elif operator=="-":
-  print(number1-number2)
+  return number1-number2
 # else if operator is "*",then perform multiplication and print result
  elif operator=="*":
-  print(number1*number2)
+  return number1*number2
 # else if operator is "/",then perform division and print result
  elif operator=="/" and number2!=0:
-  print(number1/number2) 
+  return number1/number2 
 # else if operator is "//",then perform integer division and print result
  elif operator=="//" and number2!=0:
-  print(number1//number2)
+  return number1//number2
 # else if operator is "**",then perform power operation and print result
  elif operator=="**":
-  print(number1**number2)
+  return number1**number2
 # else return False
  else:
   return False

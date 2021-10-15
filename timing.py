179b66f6b3c089a1ab43596756f25efe94c514a4
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 18:23:53 -0700

    added comments and fixed syntax

--- timing.py
+++ timing.py
@@ -1,16 +1,18 @@
 import time
 
 def calculate_time(func):
+ '''decorator fuction to calculate time taken to run a functio'''
  def innermethod():
+  '''inner function of decorator to incorporate changes to the function passed'''
   begin = time.time()
   timer()
   end = time.time()
   print("total time taken: ",end-begin)
  return innermethod
-
+#defining function to be called within decorator
 def timer():
   time.sleep(2)
   
-function_used = calculate_time(timer)
-
-function_used()
+timer = calculate_time(timer)
+#calls function to time
+timer()

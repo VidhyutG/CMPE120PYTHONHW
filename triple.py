58890556bcc791162e59bb65d9a132e131270bfb
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 18:25:27 -0700

    added comments

--- triple.py
+++ triple.py
@@ -1,5 +1,7 @@
 def tripler(func):
+ '''decorator to call any function passed to it 3x'''
  def wrapper():
+  ''' inner function of decorator to call implement its changes'''
   func()
   func()
   func()
@@ -9,5 +11,5 @@ def myFunction():
   print("hello")
   
 myFunction = tripler(myFunction)
-
+#calling myFunction
 myFunction()

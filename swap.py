4de1a61f68b0d706f71adc19a4be0343fc4eba20
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 18:12:37 -0700

    made program simpler

--- swap.py
+++ swap.py
@@ -1,10 +1,6 @@
 #program to swap first and last item in a list
 def swap_last_item(newList):
  ''' Accepts a list and interchanges the first and last element in the list'''
- #length of list is assigned to size for easy index access
- size = len(newList) 
- temp = newList[0]
- newList[0] = newList[size-1]
- newlist[size-1] = temp
-  
+ #swapping
+ newList[0], newList[-1] = newList[-1], newList[0]
  return newList

5191423674cc4b90043518604ce9a2126711e52b
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-10-14 17:06:08 -0700

    created decorator for timing.py

---
+++ timing.py
@@ -0,0 +1,16 @@
+import time
+
+def calculate_time(func):
+ def innermethod():
+  begin = time.time
+  timer()
+  end = time.time
+  print("total time taken: "end-begin)
+ return innermethod
+
+def timer():
+  time.sleep(2)
+  
+function_used = calculate_time(timer)
+
+function used()
